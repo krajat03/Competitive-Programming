b123080b1f480d9ec7de2f935f9b0bcb1fccafc1
krajat03 <rajatunsar@gmail.com>
2026-01-10 15:54:16 +0530

    CodeForces 281/A

---
+++ A_Word_Capitalization.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Word_Capitalization {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        String str = fs.next();
+        char[] ch = str.toCharArray();
+
+        if (ch[0] >= 'a' && ch[0] <= 'z') {
+            ch[0] = (char) (ch[0] - 32);
+        }
+        System.out.println(new String(ch));
+        out.close();
+    }
+
+    // FastScanner class for fast input
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            try { return br.readLine(); }
+            catch (IOException e) { return null; }
+        }
+    }
+}
