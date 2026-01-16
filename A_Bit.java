45903e0c5874e481b74b088082a0f533abfff573
krajat03 <rajatunsar@gmail.com>
2026-01-16 20:45:20 +0530

    CodeForces 282A

---
+++ A_Bit.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Bit {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        solve();
+        out.close();
+    }
+
+    static void solve() {
+        int n = fs.nextInt();
+        int x = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = fs.next();
+            if (s.contains("++")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        System.out.println(x);
+    }
+
+    // FastScanner class for fast input
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+}
