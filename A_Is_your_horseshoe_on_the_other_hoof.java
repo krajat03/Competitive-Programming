d2a9e9bc68f8780f31e6798e33057cc068293561
krajat03 <rajatunsar@gmail.com>
2026-01-16 20:47:44 +0530

    CodeForces 228A

---
+++ A_Is_your_horseshoe_on_the_other_hoof.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Is_your_horseshoe_on_the_other_hoof {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        solve();
+        out.close();
+    }
+
+    static void solve() {
+        int count = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            int val = fs.nextInt();
+            if (!set.add(val)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    // FastScanner class for fast input
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+}
