54975552d55e2274ff0e9ee8af08bc892acd6426
krajat03 <rajatunsar@gmail.com>
2026-01-17 21:28:22 +0530

    CodeForces 227B

--- B_Effective_Approach.java
+++ B_Effective_Approach.java
@@ -1,30 +1,70 @@
+import java.io.*;
 import java.util.*;
 
 public class B_Effective_Approach {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        Map<Integer, Integer> position = new HashMap<>();
+        int n = fs.nextInt();
+        int[] pos = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-            position.put(arr[i], i + 1);
+        for (int i = 1; i <= n; i++) {
+            int val = fs.nextInt();
+            pos[val] = i;
         }
 
-        int m = scanner.nextInt();
-        long vasya = 0;
-        long petya = 0;
+        long vCount = 0, pCount = 0;
+        int q = fs.nextInt();
 
-        for (int i = 0; i < m; i++) {
-            int q = scanner.nextInt();
-            int pos = position.get(q);
-            vasya += pos;           
-            petya += (n - pos + 1); 
+        for (int i = 0; i < q; i++) {
+            int target = fs.nextInt();
+            int idx = pos[target];
+
+            vCount += idx;
+            pCount += (n - idx + 1);
+        }
+        System.out.println(vCount + " " + pCount);
+    }
+
+    static void solve() {
+
+    }
+
+    // FastScanner class for fast input
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
         }
 
-        System.out.println(vasya + " " + petya);
-        scanner.close();
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
     }
 }
